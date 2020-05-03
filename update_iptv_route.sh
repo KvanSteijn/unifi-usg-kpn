b7e089a4dad110232db9e8df4cf66c4bec6023ab
Kevin van Steijn <info@kvansteijn.nl>
2020-05-03 10:16:16 +0200

    Improved update script

--- update_iptv_route.sh
+++ update_iptv_route.sh
@@ -2,15 +2,23 @@
 
 source /opt/vyatta/etc/functions/script-template
 
+# Start configure
 configure
 
+# Try to get DHCP IP */
 r_ip=$(run show dhcp client leases | grep router | awk '{ print $3 }');
 iptv_static=$(echo "set protocols static route 213.75.112.0/21 next-hop $r_ip")
+if [[ $r_ip != '' ]]; then
+    # Delete old IP
+    delete protocols static route 213.75.112.0/21
 
-delete protocols static route 213.75.112.0/21
+    # Set new IP
+    eval $iptv_static
 
-eval $iptv_static
+    # Commit and save last changes
+    commit
+    save
+fi
 
-commit
-save
+# Close configure
 exit
